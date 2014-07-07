96033bddb3a75c8e1f8523590410b3e5ec4b38d0
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-07 18:40:01 -0400

    commented

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source "https://rubygems.org"
 
-gem 'rubygems'
 gem 'twilio-ruby'
 gem 'sinatra'
--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -6,9 +6,9 @@ get '/' do
   "Hello, world"
 end
 
-get '/sms-quickstart' do
-   twiml = Twilio::TwiML::Response.new do |r|
-     r.Message "Hey Amy. Thanks for the message! This finally works!!!"
-   end
-   twiml.text
- end
+# get '/sms-quickstart' do
+#    twiml = Twilio::TwiML::Response.new do |r|
+#      r.Message "Hey Amy. Thanks for the message! This finally works!!!"
+#    end
+#    twiml.text
+#  end
