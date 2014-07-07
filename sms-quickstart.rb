7960cbe8de5c69c990b32735e4ced346942ceea5
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-07 18:34:05 -0400

    added hello world to test

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
- 
+
+get '/' do
+  "Hello, world"
+end
+
 get '/sms-quickstart' do
    twiml = Twilio::TwiML::Response.new do |r|
      r.Message "Hey Amy. Thanks for the message! This finally works!!!"
