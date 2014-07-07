7145f2f1e24affa6c61f754a157c3c940aae5643
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-07 18:45:22 -0400

    added back twilio functionality

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -1,14 +1,14 @@
-#require 'rubygems'
-#require 'twilio-ruby'
+require 'rubygems'
+require 'twilio-ruby'
 require 'sinatra'
 
 get '/' do
   "Hello, world"
 end
 
-# get '/sms-quickstart' do
-#    twiml = Twilio::TwiML::Response.new do |r|
-#      r.Message "Hey Amy. Thanks for the message! This finally works!!!"
-#    end
-#    twiml.text
-#  end
+get '/sms-quickstart' do
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Hey Amy. Thanks for the message! This finally works!!!"
+    end
+    twiml.text
+  end
